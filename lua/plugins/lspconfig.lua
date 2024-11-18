50bdb871e80d3f3b135f87df0f1908de8920ac98
ritchie <ritchie46@gmail.com>
2024-11-18 20:14:06 +0100

    improve path visualization

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -63,7 +63,9 @@ return {
 				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 				-- Find references for the word under your cursor.
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gr", function()
+					require("telescope.builtin").lsp_references({ path_display = { "smart" } })
+				end, "[G]oto [R]eferences")
 
 				-- Jump to the implementation of the word under your cursor.
 				--  Useful when your language has ways of declaring types without an actual implementation.

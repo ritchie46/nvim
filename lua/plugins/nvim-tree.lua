35b85d5c6ac82efa9769fb99c9edc5dcf70f4f64
ritchie <ritchie46@gmail.com>
2024-11-18 20:10:40 +0100

    improve mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,21 +1,22 @@
-local function my_on_attach(bufnr)
-	local api = require("nvim-tree.api")
-	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	end
-	-- OR use all default mappings
-	api.config.mappings.default_on_attach(bufnr)
-	--
-	local function single_click_edit()
-		local node = api.tree.get_node_under_cursor()
-
-		if node and node.type == "directory" then
-			api.node.open.edit()
-		end
-	end
-
-	vim.keymap.set("n", "<LeftRelease>", single_click_edit, opts("Open or Toggle Directory"))
-end
+-- Attach to on_attach to modify defaults
+-- local function my_on_attach(bufnr)
+-- 	local api = require("nvim-tree.api")
+-- 	local function opts(desc)
+-- 		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+-- 	end
+-- 	-- OR use all default mappings
+-- 	api.config.mappings.default_on_attach(bufnr)
+-- 	--
+-- 	local function single_click_edit()
+-- 		local node = api.tree.get_node_under_cursor()
+--
+-- 		if node and node.type == "directory" then
+-- 			api.node.open.edit()
+-- 		end
+-- 	end
+--
+-- 	vim.keymap.set("n", "<LeftRelease>", single_click_edit, opts("Open or Toggle Directory"))
+-- end
 
 return {
 	"nvim-tree/nvim-tree.lua",
@@ -26,11 +27,11 @@ return {
 	},
 	config = function()
 		require("nvim-tree").setup({
-			on_attach = my_on_attach,
+			-- on_attach = my_on_attach,
 			view = {
 				adaptive_size = true,
 			},
-			filters = { enable = false },
+			filters = { dotfiles = false },
 		})
 	end,
 }

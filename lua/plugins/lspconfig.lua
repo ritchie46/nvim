5f05e4fac01c2825f700875ba2537d6d35a0390f
ritchie <ritchie46@gmail.com>
2024-12-20 16:02:22 +0100

    remove <leader>cd

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -96,13 +96,6 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-				vim.api.nvim_set_keymap(
-					"n",
-					"<leader>cd",
-					"<cmd>lua vim.diagnostic.open_float()<CR>",
-					{ noremap = true, silent = true, desc = "Toggle diagnostic window" }
-				)
-
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
